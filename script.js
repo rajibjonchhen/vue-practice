bceca102fbb6c7f82d3d5886863fe498ad01208c
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-09-12 13:51:05 +0100

    vue set up file created

---
+++ script.js
@@ -0,0 +1,13 @@
+Vue.createApp({
+    data(){
+        return{
+            user:"rajib"
+        }
+    }
+}).mount("header")
+
+Vue.createApp({
+    data(){
+        return{}
+    }
+}).mount("#app")
