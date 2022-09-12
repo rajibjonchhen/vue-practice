2c061cae29f356c507186fd2e43c7d57dc20da2d
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-09-12 16:30:36 +0100

    nav link and books add programatically

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 Vue.createApp({
-    data(){
-        return{
-            user:"rajib"
-        }
-    }
-}).mount("header")
+  data() {
+    return {
+      user: "Rajib",
+      links: ["Home", "About", "Books"],
+    };
+  },
+}).mount("header");
 
 Vue.createApp({
-    data(){
-        return{}
-    }
-}).mount("#app")
+  data() {
+    return {
+      books: [
+        {
+          id: 1,
+          title: "The new beginning",
+          img:"",
+          description: `
+                    What is a quote about new beginnings?
+Image result for the new beginnings
+“No matter how hard the past is, you can always begin again.” “Every day is a chance to begin again. Don't focus on the failures of yesterday, start today with positive thoughts and expectations.” “Forgiveness says you are given another chance to make a new beginning.”
+                    `,
+        },
+        {
+          id: 2,
+          title: "The second new beginning",
+          description: `
+                    What is a quote about new beginnings?
+Image result for the new beginnings
+“No matter how hard the past is, you can always begin again.” “Every day is a chance to begin again. Don't focus on the failures of yesterday, start today with positive thoughts and expectations.” “Forgiveness says you are given another chance to make a new beginning.”
+                    `,
+        },
+      ],
+    };
+  },
+}).mount("main");
